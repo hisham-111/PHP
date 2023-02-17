31e8cf9f49afdbaf598771d0909534f9fe6f588f
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-02-17 23:12:18 +0200

    basics 1 step 4

--- exercises/php-basics-01/index.php
+++ exercises/php-basics-01/index.php
@@ -1,12 +1,21 @@
 <?php
 $name = 'Hisham';
-$email = 'Hisham777@gmail.com';
-$height = 12;
-$width = 18;
-$area = $height * $width;
-// Output
-echo 'My name is ' . $name . ' and my email: ' . $email;
-print "\n";  
-echo 'The area : ' . $area ;
+$position = ' front end developer';
+$github_url = 'https://github.com/gk-git';
+
+//first method 
+echo "Hello Im " . $name .", Im a" . $position . "check my github " .$github_url;
+echo'<br/>';
+//second method
+echo "Hello Im  {$name}, Im a {$position}  check my github  {$github_url}";
+echo'<br/>';
+//third method 
+echo 'Hello Im ' . $name .', Im a' . $position . 'check my github ' .$github_url;
+echo'<br/>';
+//3
+
+echo "  My name is   {$name}, junior {$position} this is my {$github_url} account ";
+
+
 
 ?>
