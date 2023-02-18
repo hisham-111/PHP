bbe01207c0187a65f6faee90d44875bc098b4bf0
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-02-18 21:54:25 +0200

    basics1 step5

---
+++ exercises/Basics-01/step5.php
@@ -0,0 +1,11 @@
+<?php
+
+$course_name = 'Responsive design';
+$enrolled_students = 17;
+$price = 17.4;
+$on_discount = false;
+
+echo "Course title: {$course_name}\nEnrolled Students: {$enrolled_students}\nCourse price:  {$price}\nCourse on discount: {$on_discount}" ;
+
+
+?>
