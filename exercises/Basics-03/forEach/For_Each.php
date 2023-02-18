495412ac6efae4ab061ed2c143edf27658678da6
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-02-18 21:46:05 +0200

    basics 3 forEach EX

---
+++ exercises/Basics-03/forEach/For_Each.php
@@ -0,0 +1,6 @@
+<?php
+$personal_details=array("name" => "Rajesh Rao", "occupation" => "Engineer", 'age' => 39, "country" => "India");
+foreach ( $personal_details as $key => $value )
+{
+echo "$key=$value\n";
+}
