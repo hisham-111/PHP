81a2e910a6342c2d5db2c4f45da7e1ce3b2e5bb2
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-02-18 21:51:33 +0200

    basics 1 step3

---
+++ exercises/Basics-01/step3.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = 'Hisham';
+$email = 'Hisham777@gmail.com';
+$height = 12;
+$width = 18;
+$area = $height * $width;
+// Output
+echo 'My name is ' . $name . ' and my email: ' . $email;
+print "\n";  
+echo 'The area : ' . $area ;
+
+?>
