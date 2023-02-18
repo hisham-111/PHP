3042659832839a5e4bb789aafd867fd5034a3d6e
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-02-18 21:56:58 +0200

    basic 1 step 7

---
+++ exercises/Basics-01/step7.php
@@ -0,0 +1,16 @@
+<?php
+
+$birth_year = 2005;
+$current_year = 2023;
+
+$calculation  = $current_year - $birth_year;
+
+if($calculation > 18){
+    echo "You can drive" ;
+}
+else {
+    echo "You still a kid, go and play GTA";
+}
+?>
+
+?>
