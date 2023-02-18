5d100686228002e1d68952bde0e265f8ccc36b37
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-02-18 13:15:30 +0200

    basics 2 step_3

---
+++ exercises/Basics-02/step_3.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$var1 = 32;
+$var2 = 45;
+
+$var101 = $var2;
+$var202 = $var1;
+
+$var1 = $var101;
+
+$var2 = $var202;
+
+
+echo 'Value of Var1:' . $var1  . "\n";  
+echo "Value of Var2:" .  $var2 ."\n"; 
+
+
+
+
+?>
