38aed8444412cdab7508601d4425f703ddae0ae1
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-02-18 21:58:36 +0200

    basics 1 step 8-b

---
+++ exercises/Basics-01/step8-b.php
@@ -0,0 +1,27 @@
+<?php
+
+$marks = array(
+
+
+    "Eggs" => array(
+        "balade" ,
+        "mazere3" ,
+    ),
+
+    "milk" => array(
+        "Fresh",
+        "Taanayel",
+
+    ),
+    "water-pack" => array(
+        "Tanoureen",
+        "Reem",
+
+    ),
+);
+echo "Hey Sir, Please I need 1 pack of ";
+
+echo $marks['Eggs'][0]; 
+echo "eggs and 3 {$marks['water-pack'][1]} water-pack"
+
+?>
