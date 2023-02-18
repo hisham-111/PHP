ad32b612b31b6cebed74af9ca3e2133e1f64fceb
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-02-18 20:52:39 +0200

    basics 3 Ex3

---
+++ exercises/Basics-03/whileLoop/Ex3.php
@@ -0,0 +1,16 @@
+<?php
+
+// $text=" Hello best programmer ";
+$str = "Hello world";
+$i = 0;
+while($i < $str){
+    $newStr = explode(" ", $str);
+    print_r($newStr); 
+    break;
+    
+}
+
+
+
+
+?>
